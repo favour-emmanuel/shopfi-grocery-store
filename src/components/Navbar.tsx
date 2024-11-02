38a45d3eb4996c922413ed42d96429268edfaaa3
Favour Emmanuel <iyenefavour@gmail.com>
2024-11-02 17:26:44 +0100

    designed navbar icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,6 +6,8 @@ import { navItems } from "../data/data";
 
 const Navbar = () => {
   const [activeIndex, setActiveIndex] = useState<number | null>(null);
+  const [likeCount, setLikeCount] = useState(0);
+  const [cartCount, setCartCount] = useState(0);
 
   const handleMouseEnter = (index: number) => {
     setActiveIndex(index);
@@ -22,7 +24,7 @@ const Navbar = () => {
         <div className="bg-[#7C9E77] py-2.5 px-3 rounded-md">
           <Icon icon="pepicons-pop:menu" className="text-xl" />
         </div>
-        <div className="lg:w-[8rem]">
+        <div className="lg:w-[8rem] w-[6.8rem]">
           <Link to={"/"}>
             <img src={logo} alt="" />
           </Link>
@@ -63,24 +65,30 @@ const Navbar = () => {
       </div>
       {/* icons */}
       <div className="flex items-center gap-4">
-        <div className="flex gap-2">
+        <div className="hidden md:flex gap-2">
           <div className="bg-[#7C9E77] h-12 w-12 flex justify-center items-center rounded-full">
             <Icon icon="octicon:person-24" className="text-lg" />
           </div>
           <div className="text-[16px]">
-            <h3 className=" mb-0">hello</h3>
+            <h3 className=" mb-0 text-[#BFCFBD] font-medium">Hello</h3>
             <Link to={"/login"} className="font-medium">
               Login
             </Link>
           </div>
         </div>
-        <div className="bg-[#7C9E77] h-11 w-11 flex justify-center items-center rounded-full cursor-pointer">
+        <div className="relative bg-[#7C9E77] h-11 w-11 hidden md:flex justify-center items-center rounded-full cursor-pointer">
           <Icon icon="icon-park-outline:like" className="text-lg" />
+          <span className="absolute top-1  bg-white text-appBlack text-xs font-bold rounded-full h-6 w-6 flex justify-center items-center transform translate-x-1/2 -translate-y-1/2">
+            {likeCount}{" "}
+          </span>
         </div>
-        <div className="bg-[#7C9E77] h-11 w-11 flex justify-center items-center rounded-full cursor-pointer">
+        <div className="relative bg-[#7C9E77] h-11 w-11 flex justify-center items-center rounded-full cursor-pointer">
           <Icon icon="solar:bag-4-linear" className="text-lg" />
+          <span className="absolute top-1  bg-white text-appBlack text-xs font-bold rounded-full h-6 w-6 flex justify-center items-center transform translate-x-1/2 -translate-y-1/2">
+            {cartCount}{" "}
+          </span>
         </div>
-        <div className="bg-[#7C9E77] h-11 w-11 flex justify-center items-center rounded-full cursor-pointer">
+        <div className="bg-[#7C9E77] h-11 w-11 hidden md:flex justify-center items-center rounded-full cursor-pointer">
           <Icon icon="tdesign:search" className="text-lg" />
         </div>
       </div>
